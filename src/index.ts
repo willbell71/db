cb1e2e71c4acaf2d4e4c3a90592b57489cb63fef
willbell71 <williambell@bustermachine3.home>
2019-12-28 16:00:05 +0000

    Feature: added mongo objectid type

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,5 @@ export { TDBServiceValue } from './tdb-service-value';
 
 export { DBServiceFactory } from './db-service-factory';
 
+export { TMongoDBSchemaObjectId } from './mongo/tmongo-db-schema-object-id';
 export { MongoDBService } from './mongo/mongo-db-service';
---
+++ src/mongo/tmongo-db-schema-object-id.ts
@@ -0,0 +1,3 @@
+import * as mongoose from 'mongoose';
+
+export type TMongoDBSchemaObjectId = mongoose.Schema.Types.ObjectId;
